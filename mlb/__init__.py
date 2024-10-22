f65aa2a263aeb861ca2063e0e5d55fab7c6b0813
Will Sheffler <willsheffler@gmail.com>
2024-10-21 17:26:36 -0700

    ci fix

--- mlb/__init__.py
+++ mlb/__init__.py
@@ -1,1 +1,4 @@
-from mlb import run
+import os
+from mlb import backend, fileset, frontend, models, run, tests
+
+projdir = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
---
+++ mlb/backend/__init__.py
@@ -0,0 +1,3 @@
+from mlb.backend.api import *
+from mlb.backend.server import *
+from mlb.backend.gitea import *
